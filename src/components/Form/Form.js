7952cd535a343951af4bd76312f9edfba2442791
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-05-27 00:23:44 -0400

    changed to added video with api

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,33 +5,42 @@ function Form(props) {
 
     <form className='form'>
         <h3 className='form__title'>Comments</h3>
-        <section className='form__info'>
+        <section className='form__comment' >
+
+          <section className='form__text'>
           <article className='form__image'>
           </article>
           <label className='form__label'>JOIN THE CONVERSATION</label>
           <textarea className='form__input' id="comment" name="comment" type="text" placeholder='Add a new comment' />
-        </section>
-        <section className='form__submit'>
+          </section>
+
+          <section className='form__submit'>
           <button className='form__button'>COMMENT</button>
+          </section>
+
         </section>
+
         <section className='form__comments'>
            {props.details.comments.map(data => {
             return(
-              <section className='form__info'>
-              <section className='form__name-date'>
-                <p>{data.name}</p>
-                <p>{data.timestamp}</p>
-              </section>
-              <section className='form__comment'>
-                <p>{data.comment}</p>
-              </section>
+              
+              <section className='form__container'>
+                <section className='form__picture'>
+                </section>
+                <section className='form__name-date'>
+                  <p className='form__name'>{data.name}</p>
+                  <p className='form__date'>{data.timestamp}</p>
+                </section>
+                <section className='form__text-container'>
+                  <p className='form__text'>{data.comment}</p>
+                </section>
               </section>
             )
           }
         )} 
-          </section>
-        
+        </section>
     </form>
+
     );
   }
   
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,10 +1,12 @@
 import './Info.scss'
 
+const apiKey = "?api_key=ali";
+
 function Info(props) {
     return (
 
         <section className='info'>
-            <img className='info__image' src={props.details.image} alt={props.details.title} />
+            <video className='info__video' controls src={props.details.video + apiKey} alt={props.details.title} />
             <h1>{props.details.title}</h1>
             <h3>By: {props.details.channel}</h3>
             <h3>{props.details.timestamp}</h3>
