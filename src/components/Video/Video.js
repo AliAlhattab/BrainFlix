9cd9f3b22324cc1bffcb21b17ba7c72f2e86b729
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-05-30 02:12:29 -0400

    final commmit merge to main

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -6,7 +6,6 @@ function Video(props) {
             <section className='video'>
                 <video className='video__image' controls poster={props.details.image} alt={props.details.title} />
             </section>
-    
     );
   }
   
