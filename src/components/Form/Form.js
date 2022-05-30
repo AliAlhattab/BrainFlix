90006e2044e385422dd9e99060c0a3febf9afd82
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-05-29 22:40:22 -0400

    completed mobile and tablet for forms

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,22 +6,21 @@ function Form(props) {
     return (
 
     <form className='form'>
-        <h3 className='form__title'>3 Comments</h3>
-        <section className='form__comment' >
-
-          <section className='form__text'>
-          <article className='form__image'>
-          </article>
-          <label className='form__label'>JOIN THE CONVERSATION</label>
-          <textarea className='form__input' id="comment" name="comment" type="text" placeholder='Add a new comment' />
+        <p className='form__title'>3 Comments</p>
+        <section className='form__something'>
+          <section className='form__text-container'>
+            <article className='form__image'>
+            </article>
+            <section className='form__text'>
+              <label className='form__label'>JOIN THE CONVERSATION</label>
+              <textarea className='form__input' id="comment" name="comment" type="text" placeholder='Add a new comment' />
+            </section>
           </section>
-
+          
           <section className='form__submit'>
           <button className='form__button'>COMMENT</button>
           </section>
-
         </section>
-
         <section className='form__comments'>
           
            {props.details.comments.map(data => {
@@ -38,12 +37,14 @@ function Form(props) {
               <section className='form__container'>
                 <section className='form__picture'>
                 </section>
-                <section className='form__name-date'>
-                  <p className='form__name'>{data.name}</p>
-                  <p className='form__date'>{fullDate}</p>
-                </section>
-                <section className='form__text-container'>
-                  <p className='form__text'>{data.comment}</p>
+                <section className='form__comment-info'>
+                  <section className='form__name-date'>
+                    <p className='form__name'>{data.name}</p>
+                    <p className='form__date'>{fullDate}</p>
+                  </section>
+                  <section className='form__comment-container'>
+                    <p className='form__comment'>{data.comment}</p>
+                  </section>
                 </section>
               </section>
             )
